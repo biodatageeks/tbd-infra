b605f070228ae16f7d7a0393314cf2062c408c4a
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2021-03-20 17:30:48 +0100

    Diagram fixes (#17)

--- doc/diagram-infra.py
+++ doc/diagram-infra.py
@@ -11,32 +11,33 @@ with Diagram(show=False, outformat="png",
              direction="LR",
              node_attr ={"fontsize": "14"},
              edge_attr ={"labelfontsize": "14"}):
-    storage = GCS("Google Cloud Storage")
-    with Cluster("Kubernetes cluster"):
-        with Cluster("Monitoring") as monitoring:
-            metrics = Prometheus("Prometheus")
-            metrics_push = Prometheus("Prometheus Gateway")
-            metrics_push << Edge(label="pull metrics") << metrics
-            service_monitor = CRD("ServiceMonitor")
-            service_monitor << Edge(label="watch") << metrics
-            metrics << Edge(label="query metrics") << Grafana("Grafana")
-        with Cluster("Apache Spark application") as spark:
-            Spark()
-            driver = Pod("driver")
-            driver << service_monitor
-            exec_1 = Pod("executor 1")
-            exec_2 = Pod("executor 2")
-            exec_3 = Pod("executor 3")
-            driver << exec_1
-            driver << exec_2
-            driver << exec_3
-            driver >> Edge(label="push metrics") >> metrics_push
-            driver << Edge(label="pull metrics") << metrics
-            exec_1 >> Edge(label="read/write") >> storage
-            exec_2 >> Edge(label="read/write") >> storage
-            exec_3 >> Edge(label="read/write") >> storage
-        operator = Pod("Spark Operator")
-        crd = CRD("SparkApplication")
-        crd << Edge(label="watch") << operator >> Edge(label="create") >> driver
+    with Cluster("Google Cloud") as cloud:
+        storage = GCS("Google Cloud Storage")
+        with Cluster("Kubernetes cluster"):
+            with Cluster("Monitoring") as monitoring:
+                metrics = Prometheus("Prometheus")
+                metrics_push = Prometheus("Prometheus Gateway")
+                metrics_push << Edge(label="pull metrics") << metrics
+                service_monitor = CRD("ServiceMonitor")
+                service_monitor << Edge(label="watch") << metrics
+                metrics << Edge(label="query metrics") << Grafana("Grafana")
+            with Cluster("Apache Spark application") as spark:
+                Spark()
+                driver = Pod("driver")
+                driver << service_monitor
+                exec_1 = Pod("executor 1")
+                exec_2 = Pod("executor 2")
+                exec_3 = Pod("executor 3")
+                driver << exec_1
+                driver << exec_2
+                driver << exec_3
+                driver >> Edge(label="push metrics") >> metrics_push
+                driver << Edge(label="pull metrics") << metrics
+                exec_1 >> Edge(label="read/write") >> storage
+                exec_2 >> Edge(label="read/write") >> storage
+                exec_3 >> Edge(label="read/write") >> storage
+            operator = Pod("Spark Operator")
+            crd = CRD("SparkApplication")
+            crd << Edge(label="watch") << operator >> Edge(label="create") >> driver
 
 
